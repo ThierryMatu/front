8812d14b58b94a827b533fb798ffcfe06d8b6e1d
ThierryMatu <101680040+ThierryMatu@users.noreply.github.com>
2022-06-14 16:33:39 +0400

    front end

---
+++ cst3145.js
@@ -0,0 +1,92 @@
+const lessons = [
+    {
+      id: 1,
+      subject: 'Swimming',
+      location: 'Dubai,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 4,
+      image: 'images/swimming.jpg'
+    },
+    {
+      id: 2,
+      subject: 'Soccer',
+      location: 'images/Sharjah,UAE',
+      spaces: 5,
+      price: 250,
+      stars: 4,
+      image: 'images/soccer.jpg'
+    },
+    {
+      id: 3,
+      subject: 'Football',
+      location: 'Abu Dhabi,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 5,
+      image: 'images/football.jpg'
+    },
+    {
+      id: 4,
+      subject: 'Basketball',
+      location: 'Ajman,UAE',
+      spaces: 5,
+      price: 200,
+      stars: 4,
+      image: 'images/basketball.jpg'
+    },
+    {
+      id: 5,
+      subject: 'Martial Art',
+      location: 'Al Ain, UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'images/martial art.jpg'
+    },
+    {
+      id: 6,
+      subject: 'Gaming',
+      location: 'Dubai,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 2,
+      image: 'images/Gaming.jpg'
+    },
+    {
+      id: 7,
+      subject: 'Music',
+      location: 'Kalba,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'images/music.jpg'
+    },
+    {
+      id: 8,
+      subject: 'Movies',
+      location: ' Sharjah,UAE',
+      spaces: 5,
+      price: 50,
+      stars: 4,
+      image: 'images/movies.jpg'
+    },
+    {
+      id: 9,
+      subject: 'Dancing',
+      location: 'Fujairah,UAE',
+      spaces: 5,
+      price: 150,
+      stars: 5,
+      image: 'images/dancing.jpg'
+    },
+    {
+      id: 10,
+      subject: 'Volley Ball',
+      location: 'Hatta,UAE',
+      spaces: 5,
+      price: 100,
+      stars: 3,
+      image: 'images/volleyball.jpg'
+    }
+  ]
